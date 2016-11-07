dd12f2ad321010ad5e17d450e818a7f2739febd2
sofpanos <sofpanos@hotmail.com>
2016-11-08 00:16:11 +0200

    GUI Additions
    Added Edit Category and Topic buttons and tool strip menu items
    Added Insert Category, Topic and Question Form

---
+++ insCategoryForm.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Data.SqlClient
+
+Public Class insCategoryForm
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+    End Sub
+End Class
